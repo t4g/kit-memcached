b14d2bd1509d134406abd70a15b14abd9c95d912
Kyle Younge (t4g) <dev@anvil.za.net>
2013-11-21 05:58:35 +0200

    first commit -2

---
+++ examples/basic-safe.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Basic test
+ * User: soap
+ * Date: 04/12/2013
+ * Time: 21:18
+ */
+
+
+/**
+ * Procedural
+ * =================================================
+ */
+
+
+/*
+ * Load files
+ */
+require('../vendor/autoload.php');
+
+
+
+/*
+ * Connect
+ */
+$servers  = new Kit\DB\Memcached\MemcachedConnector( 'localhost', '11211' );
+
+$cache    = new Kit\DB\Memcached\MemcachedSafe( 'my_plugin_name', $servers );
+
+
+
+/*
+ * Set a value
+ */
+$cache->add( 'test_key', 'test_value' );
+
+
+
+/*
+ * Read a value
+ */
+echo $cache->get( 'test_key' );
+
